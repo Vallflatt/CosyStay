2dbba6ffeb0e6f9624838f9170af11ff213aec20
Virginie Allflatt <vallflatt@gmail.com>
2023-02-28 13:34:16 +0100

    added validations on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :flat
+  validates :start_date, presence: true, future: true, comparison: { greater_than: :end_date }
+  validates :end_date, presence: true, future: true
 end
