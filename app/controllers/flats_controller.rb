dbf18c11cc64d6d5a877a43707095b7bddb81836
Robin Goodyear <robingoodyear6@gmail.com>
2023-03-02 16:48:31 +0100

    routing non loggedin fixed

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,39 +1,38 @@
 class FlatsController < ApplicationController
-
   before_action :set_flat, only: %i[show]
-
+  skip_before_action :authenticate_user!, only: :index
   def index
-  #   if params.has_key?(:lat) && params.has_key?(:lng) && params.has_key?(:dist)
-  #     latitude = params["lat"].to_f
-  #     longitude = params["lng"].to_f
-  #     dist = params["dist"].to_f
+    #   if params.has_key?(:lat) && params.has_key?(:lng) && params.has_key?(:dist)
+    #     latitude = params["lat"].to_f
+    #     longitude = params["lng"].to_f
+    #     dist = params["dist"].to_f
 
-  #     @flats = Flat.where([
-  #       "latitude <= ? and latitude >= ? and longitude <= ? and longitude >= ?",
-  #       latitude + dist,
-  #       latitude - dist,
-  #       longitude + dist,
-  #       longitude - dist
-  #     ])
-  #   else
-  #     @flats = Flat.limit(10)
-  #   end
-  # end
+    #     @flats = Flat.where([
+    #       "latitude <= ? and latitude >= ? and longitude <= ? and longitude >= ?",
+    #       latitude + dist,
+    #       latitude - dist,
+    #       longitude + dist,
+    #       longitude - dist
+    #     ])
+    #   else
+    #     @flats = Flat.limit(10)
+    #   end
+    # end
     if params.has_key?(:lat) && params.has_key?(:lng) && params.has_key?(:dist)
       latitude = params["lat"].to_f
       longitude = params["lng"].to_f
       dist = params["dist"].to_f
 
       @flats = Flat.all
-  # The `geocoded` scope filters only flats with coordinates
+      # The `geocoded` scope filters only flats with coordinates
       @markers = @flats.geocoded.map do |flat|
-      {
-        lat: flat.latitude,
-        lng: flat.longitude
-      }
+        {
+          lat: flat.latitude,
+          lng: flat.longitude
+        }
       end
     else
-        @flats = Flat.limit(10)
+      @flats = Flat.limit(10)
     end
   end
 
