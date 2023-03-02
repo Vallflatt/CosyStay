f1eb7589c3a5a15ecdb70840b15d796ee33fa58f
Virginie Allflatt <vallflatt@gmail.com>
2023-03-02 20:22:40 +0100

    added markers and dynamic distance and zoom

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,23 +1,36 @@
 class FlatsController < ApplicationController
   before_action :set_flat, only: %i[show]
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
+
+    @distance_choices = [10, 20, 50, 100]
+
     if params.has_key?(:lat) && params.has_key?(:lng) && params.has_key?(:dist)
       latitude = params["lat"].to_f
       longitude = params["lng"].to_f
-      dist = params["dist"].to_f
+      @distance = params["dist"].to_f
 
-      @flats = Flat.all
+      @flats = Flat.near([latitude, longitude], @distance)
       # The `geocoded` scope filters only flats with coordinates
-      @markers = @flats.geocoded.map do |flat|
+      @markers = @flats.map do |flat|
         {
           lat: flat.latitude,
-          lng: flat.longitude
+          lng: flat.longitude,
+          id: flat.id
         }
       end
+      @coordinates = [
+        longitude, latitude
+      ]
     else
+      @distance = 50
+      @markers = []
       @flats = Flat.limit(10)
+      @coordinates = [6.5761984, 46.5236672]
     end
+
+    @markers = @markers.to_json
   end
 
   def show
