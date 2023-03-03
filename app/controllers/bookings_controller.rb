7ed854a674ad3145e596895324c3d2201fe2b8ad
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-03 14:45:05 +0100

    add booking is success created 2

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,22 @@
 class BookingsController < ApplicationController
-  before_action :set_flat, only: %i[create]
+  before_action :set_flat, only: %i[new create]
   def index
     bookings = current_user.requested_bookings
     @pending_bookings = bookings.where(confirmed: false)
     @approved_bookings = bookings.where(confirmed: true)
   end
 
+  def new
+    @booking = Booking.new
+  end
+
   def create
     @booking = Booking.new(booking_params)
     @booking.flat = @flat
-    # @booking.user = current_user
+    @booking.user = current_user
 
     if @booking.save
-      redirect_to flat_bookings_path(@booking), notice: 'Booking is successfully created.'
+      redirect_to flat_bookings_path(@flat), notice: 'Booking is successfully created.'
     else
       render "flats/show", status: :unprocessable_entity
     end
@@ -25,6 +29,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :flat_id)
   end
 end
