a05dd92e5332fb3e8466cba39625065a679f79f5
Virginie Allflatt <vallflatt@gmail.com>
2023-03-03 11:05:20 +0100

    added user reference to bookings and relations with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_151836) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_095703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_151836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "total_price"
+    t.bigint "user_id"
     t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "flats", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_151836) do
   end
 
   add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
 end
